dd7ce57f28b7fff6186b29c5bac1084fc00dbda6
jasonbolanos <jason.bolanos@janeirodigital.com>
2016-02-21 22:56:39 -0600

    Modify deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,36 +9,8 @@ set :bundle_path, nil
 set :bundle_binstubs, nil
 set :bundle_flags, '--system'
 set :log_level, :info
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 namespace :deploy do
 
