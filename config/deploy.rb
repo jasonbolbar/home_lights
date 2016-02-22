5cda68c728737fe9ad043f1476b4bdba8d7812dd
jasonbolanos <jason.bolanos@janeirodigital.com>
2016-02-21 22:52:04 -0600

    Modify deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :rvm_ruby_version, '2.1.2@home_lights'
 set :bundle_path, nil
 set :bundle_binstubs, nil
 set :bundle_flags, '--system'
+set :log_level, :info
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -28,7 +29,7 @@ set :bundle_flags, '--system'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
